eaba1b2a4ffc0079970c7bfb22a827413e421bee
bdel09 <bmcdelbarrio@gmail.com>
2025-08-31 10:40:39 -0400

    Update student.py

--- student.py
+++ student.py
@@ -2,31 +2,34 @@
 #Email: benjamin.delbarrio31@myhunter.cuny.edu
 
 import turtle               
-wn = turtle.Screen()
+wn = turtle.Screen()        
 t = turtle.Turtle()
 t.pensize(5)
 t.shape("circle")
-t.color("green")
-t.forward(300)
-for i in range(2):
-    t.right(90)
-    t.forward(100)
-t.color('blue')
-t.right(90)
-t.forward(300)
-for i in range(2):
-    t.right(90)
-    t.forward(100)
-t.color("cyan")
-t.right(90)
-t.forward(300)
-for i in range(2):
-    t.right(90)
-    t.forward(100)
 t.color("red")
 t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
 t.forward(300)
+t.color("cyan")
 for i in range(2):
-  t.right(90)
-  t.forward(100)
-
+    t.left(90)
+    t.forward(100)
+t.left(90)
+t.forward(300)
+t.color("blue")
+for i in range(2):
+    t.left(90)
+    t.forward(100)
+t.left(90)
+t.forward(300)
+t.color("green")
+for i in range(2):
+    t.left(90)
+    t.forward(100)
+t.left(90)
+t.forward(300)
+t.color("red")
+wn.exitonclick()
